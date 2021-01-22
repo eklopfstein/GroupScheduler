313a064e4f6d79435889f36679682d6f3386a604
Elijah Klopfstein <Elijahklopfstein@outlook.com>
2021-01-21 21:44:53 -0500

    Initial commit

---
+++ app/src/main/java/uc/edu/klopfsea/groupscheduler/MainActivity.kt
@@ -0,0 +1,18 @@
+package uc.edu.klopfsea.groupscheduler
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import uc.edu.klopfsea.groupscheduler.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/uc/edu/klopfsea/groupscheduler/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package uc.edu.klopfsea.groupscheduler.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
