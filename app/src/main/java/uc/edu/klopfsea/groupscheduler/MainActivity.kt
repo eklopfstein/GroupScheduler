a495392e618385ecf0342fd43dd53572a7f2b2bf
Sri <surapasi@mail.uc.edu>
2021-02-22 13:32:23 -0500

    Button Functionality

--- app/src/main/java/uc/edu/klopfsea/groupscheduler/MainActivity.kt
+++ app/src/main/java/uc/edu/klopfsea/groupscheduler/MainActivity.kt
@@ -1,16 +1,27 @@
 package uc.edu.klopfsea.groupscheduler
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
 
 import androidx.appcompat.app.AppCompatActivity
+import uc.edu.klopfsea.groupscheduler.ui.main.AddEvent
 import uc.edu.klopfsea.groupscheduler.ui.main.MainFragment
-
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+
+        val addEventPageButton = findViewById<ImageButton>(R.id.addEventPageButton) as ImageButton
+
+        addEventPageButton.setOnClickListener {
+             val intent = Intent(this@MainActivity, AddEvent::class.java)
+             startActivity(intent)
+        }
+
 
             if (savedInstanceState == null) {
                 supportFragmentManager.beginTransaction()
