e7a88fd1aedce7f5ef1550e8edae75d53f493e0e
Binod Dahal <60723232+bdaha@users.noreply.github.com>
2021-03-16 20:11:58 -0400

    Added connection to storage and added swipe function to get to the other fragment.

--- app/src/main/java/uc/edu/klopfsea/groupscheduler/MainActivity.kt
+++ app/src/main/java/uc/edu/klopfsea/groupscheduler/MainActivity.kt
@@ -2,40 +2,42 @@ package uc.edu.klopfsea.groupscheduler
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.GestureDetector
+import android.view.MotionEvent
 import android.widget.CalendarView
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GestureDetectorCompat
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import uc.edu.klopfsea.groupscheduler.model.Event
 
 import uc.edu.klopfsea.groupscheduler.ui.main.AddEvent
 import uc.edu.klopfsea.groupscheduler.ui.main.MainFragment
+import uc.edu.klopfsea.groupscheduler.ui.main.ScheduleFragment
 
 
 class MainActivity : AppCompatActivity() {
-    // late initialization
+
+    private lateinit var detector: GestureDetectorCompat
+
     lateinit var recyclerView: RecyclerView
     lateinit var layoutManager: RecyclerView.LayoutManager
     lateinit var calendarView: CalendarView
 
-    // creating empty ArrayList
     val eventList = arrayListOf<Event>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
 
-        // storing ID of the image button in a variable
         val addEventPageButton = findViewById<ImageButton>(R.id.addEventPageButton)
 
-        // By ID we can use each component which id is assign in xml file use findViewById()
         calendarView = findViewById(R.id.calendarView)
         recyclerView = findViewById(R.id.recyclerView)
         layoutManager = GridLayoutManager(this, 2)
         recyclerView.layoutManager = layoutManager
 
-        // operations to be performed when user tap on the addEventPageButton button
         addEventPageButton.setOnClickListener {
             val intent = Intent(this@MainActivity, AddEvent::class.java)
             startActivity(intent)
@@ -46,5 +48,70 @@ class MainActivity : AppCompatActivity() {
                     .replace(R.id.container, MainFragment.newInstance())
                     .commitNow()
         }
+        detector = GestureDetectorCompat(this, DiaryGestureListener())
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        return if (detector.onTouchEvent(event)) {
+            true
+        } else {
+            super.onTouchEvent(event)
+        }
+    }
+
+    inner class DiaryGestureListener : GestureDetector.SimpleOnGestureListener() {
+        private val SWIPE_THRESHOLD = 100
+        private val SWIPE_VELOCITY_THRESHOLD = 100
+
+        override fun onFling(downEvent: MotionEvent?, moveEvent: MotionEvent?, velocityX: Float, velocityY: Float): Boolean {
+            var diffX = moveEvent?.x?.minus(downEvent!!.x) ?: 0.0F
+            var diffY = moveEvent?.y?.minus(downEvent!!.y) ?: 0.0F
+
+            return if (Math.abs(diffX) > Math.abs(diffY)) {
+                // either left or right swipe
+                if (Math.abs(diffX) > SWIPE_THRESHOLD && Math.abs(velocityX) > SWIPE_VELOCITY_THRESHOLD) {
+                    if (diffX > 0) {
+                        //right swipe
+                        this@MainActivity.onSwipeRight()
+                    } else {
+                        //left swipe
+                        this@MainActivity.onSwipeLeft()
+                    }
+                    true
+                } else {
+                    super.onFling(downEvent, moveEvent, velocityX, velocityY)
+                }
+            } else {
+                //either top or bottom swipe
+                if (Math.abs(diffY) > SWIPE_THRESHOLD && Math.abs(velocityY) > SWIPE_VELOCITY_THRESHOLD) {
+                    if (diffY > 0) {
+                        this@MainActivity.onSwipeTop()
+                    } else {
+                        this@MainActivity.onSwipeBottom()
+                    }
+                    true
+                } else {
+                    super.onFling(downEvent, moveEvent, velocityX, velocityY)
+                }
+                }
+        }
+    }
+
+    private fun onSwipeBottom() {
+        TODO("Not yet implemented")
+    }
+
+    private fun onSwipeTop() {
+        TODO("Not yet implemented")
+    }
+
+    private fun onSwipeLeft() {
+        TODO("Not yet implemented")
+    }
+
+    private fun onSwipeRight() {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, ScheduleFragment.newInstance())
+                    .commitNow()
     }
 }
