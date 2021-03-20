ff09851316982e39b47052ddf03ad726231b386c
Sri <surapasi@mail.uc.edu>
2021-03-19 22:59:16 -0400

    created new fragment files

---
+++ app/src/main/java/uc/edu/klopfsea/groupscheduler/ui/main/NewEventViewModel.kt
@@ -0,0 +1,7 @@
+package uc.edu.klopfsea.groupscheduler.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class NewEventViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/uc/edu/klopfsea/groupscheduler/ui/main/PlanEventViewModel.kt
@@ -0,0 +1,7 @@
+package uc.edu.klopfsea.groupscheduler.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class PlanEventViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
