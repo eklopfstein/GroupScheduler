a13a2b289b961728396b6f438e4772bbc967c027
bodensjr <70085015+bodensjr@users.noreply.github.com>
2021-03-29 16:28:29 -0400

    Removed comment from GroupEvent.kt
    The comment in this file, much like in PlannedEvents.kt, isn't necessary. This class is simple enough that someone who looks at the code would be able to understand it without explanation. Having too many comments is one of the Developers' Seven Deadly Sins, so you'll want to make sure that comments are only included when necessary.

--- app/src/main/java/uc/edu/klopfsea/groupscheduler/Events/GroupEvent.kt
+++ app/src/main/java/uc/edu/klopfsea/groupscheduler/Events/GroupEvent.kt
@@ -2,8 +2,6 @@ package uc.edu.klopfsea.groupscheduler.Events
 
 import com.google.type.DateTime
 
-/***
- * group event class with Length and Name constructors. This will help us create group events
- */
+
 class GroupEvent(var groupEventLength: DateTime, var groupEventName: String) {
 }
